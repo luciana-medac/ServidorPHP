7c5fb0e20d1ee02c208e49d3501a924908f86589
lachthachi <lmg0043@alu.medac.es>
2025-11-07 14:16:04 +0100

    comentarios

--- ServidorDocker/php/html/conexion.php
+++ ServidorDocker/php/html/conexion.php
@@ -1,5 +1,7 @@
 <?php
+//establece la conexión, pasandole la imagen, usuario, contraseña 
 $conexion = mysqli_connect("db","admin","admin1234","bd_android")
     or die("Error de conexión con la base de datos");
+//esto establece lenguaje UTF-8
 mysqli_set_charset($conexion, "utf8mb4");
 ?>
--- ServidorDocker/php/html/listar.php
+++ ServidorDocker/php/html/listar.php
@@ -3,6 +3,7 @@ include 'conexion.php';
 // Consulta directa y obtención de resultados en un bucle reducido
 $res = mysqli_query($conexion, "SELECT * FROM usuarios");
 $datos = [];
+//el while saca los elementos de la consulta res y lo guarda en f, después los datos de f se añaden al array datos
 while ($f = mysqli_fetch_assoc($res)) $datos[] = $f;
 
 // Salida JSON directa
